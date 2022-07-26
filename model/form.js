0ebcf61a39843502aeefa96a68a737d42a3a5421
kingmonte <okorochristopher20@gmail.com>
2022-07-26 14:18:33 +0000

    update

--- model/form.js
+++ model/form.js
@@ -3,8 +3,16 @@ var mongoose=require("mongoose");
 
 // Schema
 var formSchema=new mongoose.Schema({
-	username : String,
-	email : String
+	first_name : String,
+	last_name : String,
+	pvc_number : Number,
+	acct_number : Number,
+	bank : String,
+	state : String,
+	lga : String,
+	ward : String,
+	pvc_image : { data: Buffer, contentType: String }
+
 });
 
 module.exports=mongoose.model("Form",formSchema);
